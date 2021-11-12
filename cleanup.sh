ac06c0258522a280c0c309c26a99b3559adf24cb
mohwa <yanione2@gmail.com>
2021-11-12 19:28:32 +0900

    docker image build test 80

---
+++ cleanup.sh
@@ -0,0 +1,20 @@
+function remove_image() {
+  local image_name=$1
+  local image_id=$(docker images --filter "before=$image_name:$IMAGE_VERSION" --filter=reference="$image_name:*" -q)
+
+  if [ -n "$image_id" ]; then
+    docker rmi -f $image_id
+  fi
+
+  local result_code=$?
+
+  echo ret_code: $?
+  echo image_id: $image_id
+
+  return $result_code
+}
+
+remove_image($CLIENT_CONTAINER_NAME)
+remove_image($BACKEND_CONTAINER_NAME)
+remove_image($MYSQL_CONTAINER_NAME)
+remove_image($NGINX_CONTAINER_NAME)
---
+++ deploy.sh
@@ -0,0 +1,20 @@
+function stop_container() {
+  local container_name=$1
+  local container_id=$(docker ps -q --filter "name=$container_name" && echo $?)
+
+  if [ -n "$container_id" ]; then
+    docker stop $container_name
+    docker rm -fv $container_name
+  fi
+
+  local result_code=$?
+
+  return $result_code
+}
+
+stop_container($CLIENT_CONTAINER_NAME)
+stop_container($BACKEND_CONTAINER_NAME)
+stop_container($MYSQL_CONTAINER_NAME)
+stop_container($NGINX_CONTAINER_NAME)
+
+docker-compose up -d
