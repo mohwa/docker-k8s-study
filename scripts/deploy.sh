ee70b364909f4f0f6240a09f4b932df8130ba6bb
mohwa <yanione2@gmail.com>
2021-11-16 19:18:08 +0900

    Fix deploy shell 10

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,12 +18,13 @@ function stop_container() {
   return $result_code
 }
 
-stop_container $CLIENT_CONTAINER_NAME
-stop_container $BACKEND_CONTAINER_NAME
-stop_container $MYSQL_CONTAINER_NAME
-stop_container $NGINX_CONTAINER_NAME
+#stop_container $CLIENT_CONTAINER_NAME
+#stop_container $BACKEND_CONTAINER_NAME
+#stop_container $MYSQL_CONTAINER_NAME
+#stop_container $NGINX_CONTAINER_NAME
 
 # workspace root path 로 이동한다.
 cd $GITHUB_WORKSPACE
 
-docker-compose -f ./docker-compose.yml up -d --no-recreate
+#docker-compose -f ./docker-compose.yml up -d --no-recreate
+docker-compose -f ./docker-compose.yml up -d
