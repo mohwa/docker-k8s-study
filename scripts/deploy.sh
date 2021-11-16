4cad63ebd88b6e4370bb48450474c99d7aa71a76
mohwa <yanione2@gmail.com>
2021-11-16 19:14:23 +0900

    Fix deploy shell 9

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,3 +22,8 @@ stop_container $CLIENT_CONTAINER_NAME
 stop_container $BACKEND_CONTAINER_NAME
 stop_container $MYSQL_CONTAINER_NAME
 stop_container $NGINX_CONTAINER_NAME
+
+# workspace root path 로 이동한다.
+cd $GITHUB_WORKSPACE
+
+docker-compose -f ./docker-compose.yml up -d --no-recreate
