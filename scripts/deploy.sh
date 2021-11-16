7a8ecd6b0f479e464d70bb5e5743b03a396e3c92
mohwa <yanione2@gmail.com>
2021-11-16 16:33:06 +0900

    Fix deploy shell 3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,8 +22,3 @@ stop_container $CLIENT_CONTAINER_NAME
 stop_container $BACKEND_CONTAINER_NAME
 stop_container $MYSQL_CONTAINER_NAME
 stop_container $NGINX_CONTAINER_NAME
-
-# workspace root path 로 이동한다.
-cd $GITHUB_WORKSPACE
-
-docker-compose -f ./docker-compose.yml up -d
