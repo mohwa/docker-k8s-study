c7b3e1393fdd826ca929a9ba9f9be761d8bb56bb
mohwa <yanione2@gmail.com>
2021-11-21 22:29:26 +0900

    Fix deploy shell 41

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,7 +2,7 @@
 
 source $GITHUB_WORKSPACE/scripts/utils.sh
 
-#remove_image $CLIENT_IMAGE_NAME
-#remove_image $BACKEND_IMAGE_NAME
-#remove_image $MYSQL_IMAGE_NAME
-#remove_image $NGINX_IMAGE_NAME
+remove_image $CLIENT_IMAGE_NAME
+remove_image $BACKEND_IMAGE_NAME
+remove_image $MYSQL_IMAGE_NAME
+remove_image $NGINX_IMAGE_NAME
