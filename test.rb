91359098476c10eb20e6e5a4b3ccacbe92c4a0d5
mohwa <yanione2@gmail.com>
2021-11-23 12:26:37 +0900

    Add k8s settings

---
+++ test.rb
@@ -0,0 +1,17 @@
+def hello_world(v = '', &block)
+    puts v
+    block.call v
+end
+
+hello_world 'test' do | vv |
+ puts vv
+end
+
+def test_args(v = '', vv = {}, vvv = {})
+    puts v
+    puts vv
+    puts vvv
+end
+
+test_args  "private_network", ip: "192.168.1.10#"
+test_args  "private_network", { ip: "192.168.1.10#" }, { ip: "172.168.1.10#" }
