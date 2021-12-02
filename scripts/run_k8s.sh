f16bf56c4f834ac8924dfcdbd81aa4249e2bb6d2
mohwa <yanione2@gmail.com>
2021-12-02 09:59:35 +0900

    Add find command 143

--- scripts/run_k8s.sh
+++ scripts/run_k8s.sh
@@ -4,13 +4,8 @@
 # password 에는 ghcr 패키지용 토큰을 설정한다.
 # https://kubernetes.io/docs/tasks/configure-pod-container/configure-service-account/#add-imagepullsecrets-to-a-service-account
 # https://loft.sh/blog/docker-compose-to-kubernetes-step-by-step-migration/
-#secret_name=$(kubectl get secret k8s-ghcr -o name 2>/dev/null)
 
-# 반환된 secret_name 값이 있다면
-#if [ -n "$secret_name" ]; then
 kubectl delete secret k8s-ghcr
-#fi
-
 # github token 때문에, kube secret 이 생성되지않는다.
 # 이 부분도 확인해야한다.
 kubectl create secret docker-registry k8s-ghcr \
@@ -21,8 +16,6 @@ kubectl create secret docker-registry k8s-ghcr \
 
 # default serviceaccount 에 생성된 imagePullSecrets 를 추가한다.
 kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "k8s-ghcr"}]}'
-
-# echo ${{ secrets.GHCR_TOKEN }} | docker login https://ghcr.io -u mohwa --password-stdin
 
 # install kompose
 curl -L https://github.com/kubernetes/kompose/releases/download/v1.24.0/kompose-linux-amd64 -o kompose
@@ -37,9 +30,6 @@ kompose -f ../docker-compose.yml convert --replicas 3
 # 아래처럼, deployment, service 를 중지시킨 후, 배포하는것이 아닌, kubectl set 으로 pod 에 포함된 container(name으로) 이미지들만 Rolling 업데이트하는 방식으로 변경해야한다.
 # 일단 아래 순서로 재배포하자
 
-# 기존 pv 를 삭제한다.
-#pv_name=$(kubectl get pv mysql-data-pv-volume -o name 2>/dev/null)
-
 # delete deploy
 # delete pods(deploy 가 삭제되면 바인딩된 모든 pod 들도 같이 삭제(terminate)되지만, 명시적으로 삭제해주는것이 순서 보장에 좋을듯하다)
 # delete svc
@@ -53,7 +43,7 @@ kubectl delete pv mysql-data-pv-volume
 kubectl delete pvc mysql-data
 
 # persistentVolumeClaim 생성전에 persistentVolume 을 먼저 생성해야한다.
-kubectl create -f mysql-data-persistentvolume.yaml
+kubectl apply -f mysql-data-persistentvolume.yaml
 # 모든 설정들을 반영한다.
 kubectl apply -f .
 kubectl get svc
