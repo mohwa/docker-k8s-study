979838df28df80538b8844c6e40e12e6336a5821
mohwa <yanione2@gmail.com>
2021-12-02 14:04:01 +0900

    Add find command 162

--- scripts/run_k8s.sh
+++ scripts/run_k8s.sh
@@ -54,37 +54,17 @@ kubectl apply -f mysql-data-persistentvolumeclaim.yaml
 
 kubectl get svc
 
-## 서비스 바인딩된 경우
-#if [ -n "$client_service_name" ]; then
-#  # 모든 deployment 를 롤링 업데이트한다.
-#  kubectl set image client client=$CLIENT_IMAGE_NAME
-#  kubectl set image backend backend=$BACKEND_IMAGE_NAME
-#  kubectl set image mysql mysql=$MYSQL_IMAGE_NAME
-#  kubectl set image nginx nginx=$NGINX_IMAGE_NAME
-#else
-#  # default serviceaccount 에 생성된 imagePullSecrets 를 추가한다.
-#  # kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "k8s-ghcr"}]}'
-#
-#  # find -name "*.yaml" -exec sed -i "s/extensions\/v1beta1/apps\/v1/g" {} \;
-#
-#  # 아래처럼, deployment, service 를 중지시킨 후, 배포하는것이 아닌, kubectl set 으로 pod 에 포함된 container(name으로) 이미지들만 Rolling 업데이트하는 방식으로 변경해야한다.
-#  # 일단 아래 순서로 재배포하자
-#
-#  # delete deploy
-#  # delete pods(deploy 가 삭제되면 바인딩된 모든 pod 들도 같이 삭제(terminate)되지만, 명시적으로 삭제해주는것이 순서 보장에 좋을듯하다)
-#  # delete svc
-#  # pvc / pv 를 삭제하기위해, 위 과정처럼, 바인딩된 deploy, service 를 먼저 지워줘야한다(이는 임시방편이고, 롤링 업데이트 방식으로 바꿔야한다)
-#  # delete pvc
-#  # delete pv
+#  delete deploy
+#  delete pods(deploy 가 삭제되면 바인딩된 모든 pod 들도 같이 삭제(terminate)되지만, 명시적으로 삭제해주는것이 순서 보장에 좋을듯하다)
+#  delete svc
+#  pvc / pv 를 삭제하기위해, 위 과정처럼, 바인딩된 deploy, service 를 먼저 지워줘야한다(이는 임시방편이고, 롤링 업데이트 방식으로 바꿔야한다)
+#  delete pvc
+#  delete pv
 #  kubectl delete deploy client backend mysql nginx
 #  kubectl delete svc client backend mysql nginx
-#  # pvc < pv 순서로 삭제해야한다.
+#  pvc < pv 순서로 삭제해야한다.
 #  kubectl delete pvc mysql-data
 #  kubectl delete pv mysql-data-pv-volume
-#
-#  # persistentVolumeClaim 생성전에 persistentVolume 을 먼저 생성해야한다.
+
+#  persistentVolumeClaim 생성전에 persistentVolume 을 먼저 생성해야한다.
 #  kubectl apply -f mysql-data-persistentvolume.yaml
-#  # 모든 설정들을 반영한다.
-#  kubectl apply -f .
-#  kubectl get svc
-#fi
