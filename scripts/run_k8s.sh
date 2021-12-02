46d6f44af837a8d1b563a873101367ab0364546f
mohwa <yanione2@gmail.com>
2021-12-02 09:52:30 +0900

    Add find command 142

--- scripts/run_k8s.sh
+++ scripts/run_k8s.sh
@@ -4,12 +4,12 @@
 # password 에는 ghcr 패키지용 토큰을 설정한다.
 # https://kubernetes.io/docs/tasks/configure-pod-container/configure-service-account/#add-imagepullsecrets-to-a-service-account
 # https://loft.sh/blog/docker-compose-to-kubernetes-step-by-step-migration/
-secret_name=$(kubectl get secret k8s-ghcr -o name 2>/dev/null)
+#secret_name=$(kubectl get secret k8s-ghcr -o name 2>/dev/null)
 
 # 반환된 secret_name 값이 있다면
-if [ -n "$secret_name" ]; then
-  kubectl delete secret k8s-ghcr
-fi
+#if [ -n "$secret_name" ]; then
+kubectl delete secret k8s-ghcr
+#fi
 
 # github token 때문에, kube secret 이 생성되지않는다.
 # 이 부분도 확인해야한다.
@@ -34,25 +34,25 @@ cd k8s_config
 kompose -f ../docker-compose.yml convert --replicas 3
 # find -name "*.yaml" -exec sed -i "s/extensions\/v1beta1/apps\/v1/g" {} \;
 
-# 상황에 따라, deployment(pods), service 를 재배포하는것이 아닌, kubectl set 으로 pod 에 포함된 container(name으로) 이미지만 Rolling 업데이트할 수 있다.
+# 아래처럼, deployment, service 를 중지시킨 후, 배포하는것이 아닌, kubectl set 으로 pod 에 포함된 container(name으로) 이미지들만 Rolling 업데이트하는 방식으로 변경해야한다.
+# 일단 아래 순서로 재배포하자
 
 # 기존 pv 를 삭제한다.
-pv_name=$(kubectl get pv mysql-data-pv-volume -o name 2>/dev/null)
+#pv_name=$(kubectl get pv mysql-data-pv-volume -o name 2>/dev/null)
 
-# 반환된 pvc_name 값이 있다면
-if [ -n "$pv_name" ]; then
-  kubectl delete pv mysql-data-pv-volume
-fi
+# delete deploy
+# delete pods(deploy 가 삭제되면 바인딩된 모든 pod 들도 같이 삭제(terminate)되지만, 명시적으로 삭제해주는것이 순서 보장에 좋을듯하다)
+# delete svc
+# pvc / pv 를 삭제하기위해, 위 과정처럼, 바인딩된 deploy, service 를 먼저 지워줘야한다(이는 임시방편이고, 롤링 업데이트 방식으로 바꿔야한다)
+# delete pvc
+# delete pv
+kubectl delete deploy --all
+kubectl delete pods --all
+kubectl delete svc --all
+kubectl delete pv mysql-data-pv-volume
+kubectl delete pvc mysql-data
 
-# 기존 pvc 를 삭제한다.
-pvc_name=$(kubectl get pvc mysql-data -o name 2>/dev/null)
-
-# 반환된 pvc_name 값이 있다면
-if [ -n "$pvc_name" ]; then
-  kubectl delete pvc mysql-data
-fi
-
-# persistentVolumeClaim 생성전에 persistentVolume 을 먼저 생성한다.(또는 해야한다)
+# persistentVolumeClaim 생성전에 persistentVolume 을 먼저 생성해야한다.
 kubectl create -f mysql-data-persistentvolume.yaml
 # 모든 설정들을 반영한다.
 kubectl apply -f .
